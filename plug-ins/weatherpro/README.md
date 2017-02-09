ecf533234f5dc20f266f2983eb9039d3a90763ff
Hsyneve <906592157@qq.com>
2017-02-09 11:26:14 +0800

    1

---
+++ plug-ins/weatherpro/README.md
@@ -0,0 +1,7 @@
+
+## 安装
+##```$ npm install -g md-weather```
+## 查询本地天气命令
+## ```$ getweather```
+## 查询当前所在城市命令
+## ```$ getcity```
---
+++ plug-ins/weatherpro/bin/city.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+var $ = require('http');
+function getCity(data) {
+    console.log("您好，您现在所在的城市为 "+data);
+}
+
+
+
+$.get('http://api.jirengu.com/city.php', (res) => {
+            var resData = "";
+            res.on('data', (chunk) => {
+                resData += `${chunk}`;
+            });
+            res.on("end", function() {
+                    getCity(resData);
+                    });
+
+                res.resume();
+            }).on('error', (e) => {
+            console.log(`Got error: ${e.message}`);
+        });
+   
